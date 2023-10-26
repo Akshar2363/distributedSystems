41cb0aa281563c44f2c6cd2be7120ca7e4a0dd26
Gaurav Kumar Singh <110986075+Shinobi0607@users.noreply.github.com>
2023-10-26 23:02:32 +0530

    Update RMI_ServerSumN.java
    Completed the code

--- RMI_ClientServer/RMI_ServerSumN.java
+++ RMI_ClientServer/RMI_ServerSumN.java
@@ -1,5 +1,29 @@
 package RMI_ClientServer;
 
-public class RMI_ServerSumN {
-    
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMI_ServerSumN extends UnicastRemoteObject implements RMI_InterfaceSumN {
+    public RMI_ServerSumN() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public double sumUptoN(double n) throws RemoteException {
+        double sum = (n * (n + 1)) / 2;
+        System.out.println("Sum of numbers up to " + n + " is: " + sum);    
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(8888);
+            registry.bind("sumn", new RMI_ServerSumN());
+            System.out.println("The RMI Sum App is running and ready...");
+        } catch (Exception e) {
+            System.out.println("Error: The RMI Sum App is not running...");
+        }
+    }
 }
