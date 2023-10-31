fc74e08734851ab0e4ad1b38c3f5ca0b88cf701f
Gaurav Kumar Singh <110986075+Shinobi0607@users.noreply.github.com>
2023-10-31 13:34:39 +0530

    Update RMI_InterfaceSumN.java

--- RMI_ClientServer/RMI_InterfaceSumN.java
+++ RMI_ClientServer/RMI_InterfaceSumN.java
@@ -1,8 +1,5 @@
 package RMI_ClientServer;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 
 public interface RMI_InterfaceSumN extends Remote{
-    public double sumUptoN(double input) throws RemoteException;
 }
