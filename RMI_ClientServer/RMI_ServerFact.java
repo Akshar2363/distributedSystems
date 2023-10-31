16ff40f58a47b7e96b95f5a9b3135647cda52a4c
Gaurav Kumar Singh <110986075+Shinobi0607@users.noreply.github.com>
2023-10-31 13:33:18 +0530

    Updated RMI_ServerFact.java

--- RMI_ClientServer/RMI_ServerFact.java
+++ RMI_ClientServer/RMI_ServerFact.java
@@ -1,5 +1,33 @@
 package RMI_ClientServer;
 
-public class RMI_ServerFact {
-    
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMI_ServerFact extends UnicastRemoteObject implements RMI_InterfaceFact {
+    public RMI_ServerFact() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public double computeFactorial(double n) throws RemoteException {
+
+        double fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + n + " is: " + fact); 
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(7777);
+            registry.bind("fact", new RMI_ServerFact());
+            System.out.println("The RMI Factorial App is running and ready...");
+        } catch (Exception e) {
+            System.out.println("Error: The RMI Factorial App is not running...");
+        }
+    }
 }
