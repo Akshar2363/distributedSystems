a6a181bf134570090f67cc822c2433c6862e570d
theyogender <badgujjaryogender@gmail.com>
2023-10-31 23:14:57 +0530

    Creating a function in RMI_ServerSumN.java for sum upto N natural numbers

---
+++ RMI_ServerSumN.java
@@ -0,0 +1,30 @@
+
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+
+
+public class RMI_ServerSumN extends UnicastRemoteObject implements RMI_InterfaceSumN{
+    public RMI_ServerSumN() throws RemoteException{
+        super();
+    }
+
+    @Override
+    public double sumUptoN(double n) throws RemoteException{
+        return (n*(n+1))/2;
+    }
+
+    public static void main(String[] args) {
+        try{
+            Registry registry = LocateRegistry.createRegistry(8888); 
+            registry.bind("sumn", new RMI_ServerSumN());
+            System.out.println("The RMI Sum App is running and ready...");
+        }catch(Exception e){
+            System.out.println("Error : The RMI Sum App is not running...");
+
+        }
+    }
+}
